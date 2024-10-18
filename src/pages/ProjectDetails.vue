42f5abf264028715ca92497f0052434d791e3fff
ToanJt <lexuantoan5703@gmail.com>
2024-10-19 02:20:16 +0700

    complete

--- src/pages/ProjectDetails.vue
+++ src/pages/ProjectDetails.vue
@@ -65,49 +65,52 @@ function getUrl() {
 </script>
 
 <template>
-    <div class="pb-40 pt-60 w-1286 mx-auto">
-        <h1 class="text-6xl sofia-medium">{{ projectDetails.name }}</h1>
-        <div class="flex mt-16">
-            <div class="mr-16">
-                <p class="text-xl sofia-medium mb-3">Client:</p>
-                <p class="text-xl sofia-medium mb-3">Year:</p>
-                <p class="text-xl sofia-medium mb-3">Size:</p>
-                <p class="text-xl sofia-medium mb-3">Description:</p>
-            </div>
-            <div>
-                <p class="text-lg sofia-light mb-3 text-[#727272]">{{ projectDetails.client }}</p>
-                <p class="text-lg sofia-light mb-3 text-[#727272]">{{ projectDetails.year }}</p>
-                <p class="text-lg sofia-light mb-3 text-[#727272]">{{ projectDetails.size }}</p>
-                <p class="text-lg sofia-light mb-3 text-[#727272]">{{ projectDetails.description }}</p>
-            </div>
-        </div>
-        <ul class="flex gap-6 text-2xl pt-8 pb-6">
-            <Icon class="hoverable cursor-pointer hover:text-blue-600 transition-all" icon="ic:baseline-facebook"
-                width="1em" height="1em" />
-            <Icon class="hoverable cursor-pointer hover:text-green-600 transition-all" icon="ic:baseline-whatsapp"
-                width="1em" height="1em" />
-            <Icon class="hoverable cursor-pointer hover:text-pink-600 transition-all" icon="dashicons:instagram"
-                width="1em" height="1em" />
-            <Icon class="hoverable cursor-pointer hover:text-green-600 transition-all" icon="fa6-brands:square-upwork"
-                width="1em" height="1em" />
-
-            <div class="flex cursor-pointer relative copyLink">
-                <i @click="getUrl()" class="text-sm fa-solid fa-link"></i>
-                <div
-                    class="absolute transition-opacity duration-500 bottom-6 w-10 h-4 bg-vr-light-gray rounded-full flex items-center justify-center">
-                    <p class="text-[8px]">copy link</p>
+    <div class="w-screen">
+        <div class="pb-40 lg:pt-60 md:pt-52 pt-36 container mx-auto px-4">
+            <h1 class="md:text-6xl sm:text-5xl text-4xl sofia-medium">{{ projectDetails.name }}</h1>
+            <div class="flex mt-16">
+                <div class="mr-16">
+                    <p class="md:text-xl text-base sofia-medium mb-3">Client:</p>
+                    <p class="md:text-xl text-base sofia-medium mb-3">Year:</p>
+                    <p class="md:text-xl text-base sofia-medium mb-3">Size:</p>
+                    <p class="md:text-xl text-base sofia-medium mb-3">Description:</p>
+                </div>
+                <div>
+                    <p class="md:text-lg text-sm sofia-light mb-3 text-[#727272]">{{ projectDetails.client }}</p>
+                    <p class="md:text-lg text-sm sofia-light mb-3 text-[#727272]">{{ projectDetails.year }}</p>
+                    <p class="md:text-lg text-sm sofia-light mb-3 text-[#727272]">{{ projectDetails.size }}</p>
+                    <p class="md:text-lg text-sm sm:text-left text-justify sofia-light mb-3 text-[#727272]">{{
+                        projectDetails.description }}</p>
                 </div>
             </div>
-        </ul>
+            <ul class="flex gap-6 text-2xl pt-8 pb-6">
+                <Icon class="hoverable cursor-pointer hover:text-blue-600 transition-all" icon="ic:baseline-facebook"
+                    width="1em" height="1em" />
+                <Icon class="hoverable cursor-pointer hover:text-green-600 transition-all" icon="ic:baseline-whatsapp"
+                    width="1em" height="1em" />
+                <Icon class="hoverable cursor-pointer hover:text-pink-600 transition-all" icon="dashicons:instagram"
+                    width="1em" height="1em" />
+                <Icon class="hoverable cursor-pointer hover:text-green-600 transition-all"
+                    icon="fa6-brands:square-upwork" width="1em" height="1em" />
 
-        <div v-for="(image, index) in projectDetails.images" :key="index" class="mt-20 grid gap-10">
-            <img loading="lazy" :src="image" alt="">
-        </div>
-        <div class="flex">
-            <div @click="goBack()"
-                class="flex items-center gap-2 mt-16 hover:text-red-500 transition-colors duration-500 cursor-pointer">
-                <i class="text-xl fa-solid fa-arrow-left-long"></i>
-                <p class="sofia-pro">Prev Projects</p>
+                <div class="flex cursor-pointer relative copyLink">
+                    <i @click="getUrl()" class="text-sm fa-solid fa-link"></i>
+                    <div
+                        class="absolute transition-opacity duration-500 bottom-6 w-10 h-4 bg-vr-light-gray rounded-full flex items-center justify-center">
+                        <p class="text-[8px]">copy link</p>
+                    </div>
+                </div>
+            </ul>
+
+            <div class="lg:mt-20 mt-16 grid lg:gap-20 md:gap-16 sm:gap-10 gap-8">
+                <img v-for="(image, index) in projectDetails.images" :key="index" loading="lazy" :src="image" alt="">
+            </div>
+            <div class="flex">
+                <div @click="goBack()"
+                    class="flex items-center gap-2 lg:mt-16 md:mt-12 mt-8 hover:text-red-500 transition-colors duration-500 cursor-pointer">
+                    <i class="text-xl fa-solid fa-arrow-left-long"></i>
+                    <p class="sofia-pro">Prev Projects</p>
+                </div>
             </div>
         </div>
     </div>
