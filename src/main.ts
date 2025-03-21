2c1656bb9ac0d5be0a6da30177641d9fbd683c37
ToanJt <lexuantoan5703@gmail.com>
2025-03-21 10:45:18 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
+import "../text-particles.js";
 import { router } from "./router.ts";
-// import "splitting/dist/splitting.css";
-// import "splitting/dist/splitting-cells.css";
 
 import App from "./App.vue";
 const app = createApp(App);
